9bc3eae6a43b777a1f5d44e477415ee0798defc0
mithraan17 <mithraan@gmail.com>
2024-12-15 22:46:31 +0800

    Visual indication when adding a task

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,32 @@
 // src/components/TodoList.js
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoItem from './TodoItem';
 
 function TodoList({ todos, deleteTodo }) {
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      setShowNotification(true);
+      // Hide notification after 3 seconds
+      const timeoutId = setTimeout(() => {
+        setShowNotification(false);
+      }, 3000);
+
+      return () => clearTimeout(timeoutId); // Cleanup timeout on component unmount
+    }
+  }, [todos]); // Trigger whenever todos change (i.e., when a new todo is added)
+
   return (
     <div className="todo-list">
+      {/* Display notification when a new todo is added */}
+      {showNotification && (
+        <div className="notification fade-in">
+          Task Added!
+        </div>
+      )}
+
       {todos.length === 0 ? (
         <p>No todos available. Add some!</p>
       ) : (
@@ -15,7 +36,6 @@ function TodoList({ todos, deleteTodo }) {
             index={index}
             todo={todo}
             deleteTodo={deleteTodo}
-            
           />
         ))
       )}
